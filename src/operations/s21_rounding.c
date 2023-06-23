5c2de68fddc03bf7409157d7e2753e6703c72f5b
Tim Narzikulov <ntim@bk.ru>
2023-06-23 23:40:40 +0700

    s21_negate added

--- src/operations/s21_rounding.c
+++ src/operations/s21_rounding.c
@@ -15,4 +15,13 @@ int s21_truncate(s21_decimal value, s21_decimal *result) {
   return OK;
 }
 
-int s21_negate(s21_decimal value, s21_decimal *result) { return OK; }
+int s21_negate(s21_decimal value, s21_decimal *result) {
+    *result = value;
+    unsigned int mask = 1 << 31;
+    if (get_bit(value.bits[3], 31) == 0) {
+        result->bits[3] |= mask;
+    } else {
+        result->bits[3] &= ~mask;
+    }
+    return 0;
+}
