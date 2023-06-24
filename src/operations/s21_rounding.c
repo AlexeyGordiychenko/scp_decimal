95de41fb05c6b458098e39528f3cde12cedbfb6e
Tim Narzikulov <ntim@bk.ru>
2023-06-24 23:29:25 +0700

    s21_rounding.c clang-formated

--- src/operations/s21_rounding.c
+++ src/operations/s21_rounding.c
@@ -1,30 +1,31 @@
 #include "s21_common.h"
 
-int s21_floor(s21_decimal value, s21_decimal *result) { 
-    // MIN_DECIMAL_VALUE 79,228,162,514,264,337,593,543,950,335
-    // MAX_DECIMAL_VALUE 79,228,162,514,264,337,593,543,950,335
-    s21_decimal s21_min_decimal_value = {(28 << 16) | (1 << 31), -1, -1, -1};
-    s21_decimal s21_max_decimal_value = {0, -1, -1, -1};
-    s21_decimal zero_decimal = {0, 0, 0, 0};
-    s21_decimal one_decimal = {1, 0, 0, 0};
-    int error_flag = 1;
+int s21_floor(s21_decimal value, s21_decimal *result) {
+  // MIN_DECIMAL_VALUE 79,228,162,514,264,337,593,543,950,335
+  // MAX_DECIMAL_VALUE 79,228,162,514,264,337,593,543,950,335
+  s21_decimal s21_min_decimal_value = {(28 << 16) | (1 << 31), -1, -1, -1};
+  s21_decimal s21_max_decimal_value = {0, -1, -1, -1};
+  s21_decimal zero_decimal = {0, 0, 0, 0};
+  s21_decimal one_decimal = {1, 0, 0, 0};
+  int error_flag = 1;
 
-    if (s21_is_less_or_equal(value, s21_max_decimal_value) && s21_is_greater_or_equal(value, s21_min_decimal_value)) {
-        error_flag = 0;
-        s21_decimal decimal_integer_part = zero_decimal;
-        s21_truncate(value, &decimal_integer_part);
+  if (s21_is_less_or_equal(value, s21_max_decimal_value) &&
+      s21_is_greater_or_equal(value, s21_min_decimal_value)) {
+    error_flag = 0;
+    s21_decimal decimal_integer_part = zero_decimal;
+    s21_truncate(value, &decimal_integer_part);
 
-        if (s21_is_equal(value, decimal_integer_part)) {
-            *result = value;
-        } else if (s21_is_equal(value, zero_decimal)) {
-            *result = zero_decimal;
-        } else if (s21_is_less(decimal_integer_part, zero_decimal)) {
-            s21_sub(value, one_decimal, result);
-        } else if (s21_is_greater_or_equal(decimal_integer_part, zero_decimal)) {
-            *result = decimal_integer_part;
-        }
+    if (s21_is_equal(value, decimal_integer_part)) {
+      *result = value;
+    } else if (s21_is_equal(value, zero_decimal)) {
+      *result = zero_decimal;
+    } else if (s21_is_less(decimal_integer_part, zero_decimal)) {
+      s21_sub(value, one_decimal, result);
+    } else if (s21_is_greater_or_equal(decimal_integer_part, zero_decimal)) {
+      *result = decimal_integer_part;
     }
-    return error_flag; 
+  }
+  return error_flag;
 }
 
 int s21_round(s21_decimal value, s21_decimal *result) { return OK; }
@@ -41,12 +42,12 @@ int s21_truncate(s21_decimal value, s21_decimal *result) {
 }
 
 int s21_negate(s21_decimal value, s21_decimal *result) {
-    *result = value;
-    unsigned int mask = 1 << 31;
-    if (check_bit(value.bits[3], 31) == 0) {
-        result->bits[3] |= mask;
-    } else {
-        result->bits[3] &= ~mask;
-    }
-    return 0;
+  *result = value;
+  unsigned int mask = 1 << 31;
+  if (check_bit(value.bits[3], 31) == 0) {
+    result->bits[3] |= mask;
+  } else {
+    result->bits[3] &= ~mask;
+  }
+  return 0;
 }
