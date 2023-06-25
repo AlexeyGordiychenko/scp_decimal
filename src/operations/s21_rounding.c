d8ae2b85aa4f02e0fecc85bbf1dd99b3bd41e59e
Tim Narzikulov <ntim@bk.ru>
2023-06-26 01:45:36 +0700

    s21_round added

--- src/operations/s21_rounding.c
+++ src/operations/s21_rounding.c
@@ -28,7 +28,52 @@ int s21_floor(s21_decimal value, s21_decimal *result) {
   return error_flag;
 }
 
-int s21_round(s21_decimal value, s21_decimal *result) { return OK; }
+int s21_round(s21_decimal value, s21_decimal *result) {
+  s21_decimal s21_min_decimal_value = {{-1, -1, -1, 1 << 31}};
+  s21_decimal s21_max_decimal_value = {{-1, -1, -1, 0}};
+  s21_decimal zero_decimal = {{0, 0, 0, 0}};
+  s21_decimal one_decimal = {{1, 0, 0, 0}};
+  s21_decimal one_half_decimal = {{5, 0, 0, 1 << 16}};
+  s21_decimal one_half_decimal_negated = {{5, 0, 0, 1 << 16 | 1 << 31}};
+  int error_flag = 1;
+
+  if (s21_is_less_or_equal(value, s21_max_decimal_value) &&
+      s21_is_greater_or_equal(value, s21_min_decimal_value)) {
+    error_flag = 0;
+    s21_decimal decimal_integer_part = zero_decimal;
+    s21_decimal decimal_fract_part = zero_decimal;
+    s21_truncate(value, &decimal_integer_part);
+    s21_sub(value, decimal_integer_part, &decimal_fract_part);
+
+    // decimal = 0
+    if (s21_is_equal(value, zero_decimal)) {
+      *result = zero_decimal;
+    }
+    // decimal > 0
+    if (s21_is_greater(value, zero_decimal)) {
+      if (s21_is_equal(value, decimal_integer_part)) {
+        *result = decimal_integer_part;
+      } else if (s21_is_less(decimal_fract_part, one_half_decimal)) {
+        *result = decimal_integer_part;
+      } else if (s21_is_greater_or_equal(decimal_fract_part,
+                                         one_half_decimal)) {
+        s21_add(decimal_integer_part, one_decimal, result);
+      }
+    }
+    // decimal < 0
+    if (s21_is_less(value, zero_decimal)) {
+      if (s21_is_equal(value, decimal_integer_part)) {
+        *result = decimal_integer_part;
+      } else if (s21_is_greater(decimal_fract_part, one_half_decimal_negated)) {
+        *result = decimal_integer_part;
+      } else if (s21_is_less_or_equal(decimal_fract_part,
+                                      one_half_decimal_negated)) {
+        s21_sub(decimal_integer_part, one_decimal, result);
+      }
+    }
+  }
+  return error_flag;
+}
 
 int s21_truncate(s21_decimal value, s21_decimal *result) {
   int exp = get_decimal_exp(value);
