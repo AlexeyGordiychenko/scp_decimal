66c78d03993d4f60c9dcf0c79d97bf1bef5a03fa
Tim Narzikulov <ntim@bk.ru>
2023-07-21 12:03:13 +0700

    big_decimal struct moved to s21_common.h

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -9,10 +9,6 @@ typedef struct {
   unsigned int bits[4];
 } s21_decimal;
 
-typedef struct {
-  uint64_t bits[7];
-} s21_big_decimal;
-
 #include "operations/s21_arithmetic.h"
 #include "operations/s21_comparison.h"
 #include "operations/s21_converting.h"
